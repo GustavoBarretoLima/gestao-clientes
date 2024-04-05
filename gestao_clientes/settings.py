dfccdb624aacedb83122efc3b227020da6ed2521
Gustavo <gbarreto.bastos@gmail.com>
2024-04-05 13:53:13 -0300

    Configurando o heroku

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os.path
 
+from dj_database_url import parse as dburl
+from decouple import config
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@mutd!&#qq@re0xni@mfra35w9)^(*)2_pxw5ito#as!@*s+%a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gestao-clientes-4c2166a8b195.herokuapp.com']
 
 
 # Application definition
@@ -75,15 +79,10 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "gestao_clientes",
-        "USER": "postgres",
-        "PASSWORD": "Lobo5367",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
+    'default': config('DATABASE_URL',default=default_dburl, cast=dburl),
 }
 
 # Password validation
@@ -133,5 +132,7 @@ MEDIA_ROOT = 'media'
 LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = 'person_list'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
