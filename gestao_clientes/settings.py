10d5cb16343b954f370b02e2e4b3865b1bd745fe
Gustavo <gbarreto.bastos@gmail.com>
2024-04-06 12:54:01 -0300

    configurando heroku

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes-4c2166a8b195.herokuapp.com']
+ALLOWED_HOSTS = ['gestao-clientes-4dccbad81d38.herokuapp.com']
 
 # Application definition
 
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
