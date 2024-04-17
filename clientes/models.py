f35b6eaa335f17ab9472489b13582a688e638b66
Gustavo <gbarreto.bastos@gmail.com>
2024-04-17 18:04:05 -0300

    alterando media files and static files use s3

--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class Document(models.Model):
+    num_doc = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.num_doc
+
 class Person(models.Model):
     objects = None
     object = None
@@ -10,6 +16,25 @@ class Person(models.Model):
     salary = models.DecimalField(max_digits=5, decimal_places=2)
     bio = models.TextField()
     photo = models.ImageField(upload_to='clientes_photos', null=True, blank=True)
+    doc = models.OneToOneField(Document, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+class Product(models.Model):
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.description
+
+class Sale(models.Model):
+    number = models.CharField(max_length=7)
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    taxes = models.DecimalField(max_digits=5, decimal_places=2)
+    person = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
+    products = models.ManyToManyField(Product, blank=True)
+
+    def __str__(self):
+        return self.number
